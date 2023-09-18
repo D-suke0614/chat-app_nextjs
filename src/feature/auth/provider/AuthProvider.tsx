0d7d5a9f935b923131624f0cde703ac9ca46b8ce
Daisuke <d.suke.kida@gmail.com>
2023-09-19 06:51:51 +0900

    feat/ログイン状態に応じて、表示するコンポーネントの切り替え

---
+++ src/feature/auth/provider/AuthProvider.tsx
@@ -0,0 +1,47 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import type { User } from "@firebase/auth";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+
+/**
+ * undefined: 初期値
+ * null: not login状態
+ * User: login状態
+ */
+export type GlobalAuthState = {
+  user: User | null | undefined;
+};
+
+const initialState: GlobalAuthState = {
+  user: undefined,
+};
+
+const AuthContext = createContext<GlobalAuthState>(initialState);
+
+type props = { children: ReactNode };
+
+export const AuthProvider = ({ children }: props) => {
+  const [user, setUser] = useState<GlobalAuthState>(initialState);
+
+  useEffect(() => {
+    try {
+      const auth = getAuth();
+      return onAuthStateChanged(auth, (user) => {
+        setUser({
+          user,
+        });
+      });
+    } catch {
+      setUser(initialState);
+    }
+  }, []);
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
+
+export const useAuthContext = () => useContext(AuthContext);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { useAuthContext } from "@/feature/auth/provider/AuthProvider";
 import Login from "./components/login/Login";
 
 export default function Home() {
-  return <Login />;
+  const { user } = useAuthContext();
+  console.log("user", user);
+  return (
+    <div className="chatApp">
+      {user ? <h1>{`Welcome ${user.displayName}`}</h1> : <Login />}
+    </div>
+  );
 }
