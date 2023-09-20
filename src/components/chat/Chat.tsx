90d9017cb59285d8806bce891d7fe4c5430da8f0
Daisuke <d.suke.kida@gmail.com>
2023-09-21 07:30:52 +0900

    ✨ feat/チャット画面の雛形作成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { TextField, Button } from "@mui/material";
+import chatStyle from "./Chat.module.scss";
+import ChatMessage from "./ChatMessage";
 
 function Chat() {
   return (
-    <div>
-      <div>chat</div>
+    <div className={chatStyle.chat}>
+      <div className={chatStyle.chatContainer}>
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+      </div>
+      <div
+        className={chatStyle.chatInput}
+        // sx={{
+        //   width: 500,
+        //   maxWidth: "100%",
+        // }}
+      >
+        <TextField fullWidth size="small" />
+        <Button>送信</Button>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import { useAuthContext } from "@/feature/auth/provider/AuthProvider";
 import Login from "../components/login/Login";
 import Chat from "../components/chat/Chat";
 import Header from "../components/header/Header";
+import utilsStyle from "../styles/utils.module.scss";
 
 export default function Home() {
   const { user } = useAuthContext();
-  console.log("user", user);
   return (
     <>
       <Header />
-      <div className="chatApp">{user ? <Chat /> : <Login />}</div>
+      <div className={utilsStyle.chatApp}>{user ? <Chat /> : <Login />}</div>
     </>
   );
 }
