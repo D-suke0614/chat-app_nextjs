642247b3dd1253c8bfc5cb3f6b8f9b124f51e877
Daisuke <d.suke.kida@gmail.com>
2023-09-22 23:12:45 +0900

    ✨ チャットの保存と取得処理の作成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,22 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TextField, Button } from "@mui/material";
 import chatStyle from "./Chat.module.scss";
 import ChatMessage from "./ChatMessage";
-import { getDatabase, push, ref } from "firebase/database";
+import { getDatabase, push, ref, onChildAdded } from "firebase/database";
 import { FirebaseError } from "firebase/app";
+import { useAuthContext } from "@/feature/auth/provider/AuthProvider";
 
 function Chat() {
   const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const { user } = useAuthContext();
 
-  const sendChat = async () => {
+  useEffect(() => {
+    try {
+      const db = getDatabase();
+      const dbRef = ref(db, "chat");
+      return onChildAdded(dbRef, (snapshot) => {
+        const chatData = snapshot.val() ?? {};
+        console.log("fetch chatData", chatData);
+        setChats(chatData);
+      });
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.error(e);
+      }
+      return;
+    }
+  }, []);
+
+  const sendMessage = async () => {
     if (!message) return;
     console.log("message", message);
     try {
       const db = getDatabase();
       const dbRef = ref(db, "chat");
-      // await push(dbRef, {
-      //   message
-      // })
+      const chatData = {
+        message: message,
+        timestamp: "2023-09-24 15:00:00",
+        userInfo: {
+          name: user?.displayName,
+          icon: user?.photoURL,
+          uid: user?.uid,
+        },
+      };
+      console.log("chatData", chatData);
+      await push(dbRef, {
+        chatData,
+      });
       setMessage("");
     } catch (e) {
       if (e instanceof FirebaseError) {
@@ -28,6 +58,7 @@ function Chat() {
   return (
     <div className={chatStyle.chat}>
       <div className={chatStyle.chatContainer}>
+        {/* chatを表示する */}
         <ChatMessage />
         <ChatMessage />
         <ChatMessage />
@@ -50,8 +81,13 @@ function Chat() {
           size="small"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              sendMessage();
+            }
+          }}
         />
-        <Button onClick={sendChat}>送信</Button>
+        <Button onClick={sendMessage}>送信</Button>
       </div>
     </div>
   );
