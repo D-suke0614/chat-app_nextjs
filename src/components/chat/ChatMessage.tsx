d908580293330f4364484142503d47a20cfb62b9
Daisuke <d.suke.kida@gmail.com>
2023-09-23 22:30:22 +0900

    💄 チャット画面のスタイル修正

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Avatar } from "@mui/material";
 import chatMessageStyle from "./ChatMessage.module.scss";
 import Image from "next/image";
+import { useAuthContext } from "@/feature/auth/provider/AuthProvider";
 
 interface Props {
   chat: {
@@ -19,18 +20,40 @@ interface Props {
 
 function ChatMessage({ chat }: Props) {
   const { message, timestamp, userInfo } = chat.chatData;
+  const { user } = useAuthContext();
   console.log("chat", chat);
+  console.log("user", user);
   return (
-    <div className={chatMessageStyle.message}>
-      <Avatar />
-      <div className={chatMessageStyle.messageInfo}>
-        <h4>
-          {userInfo?.name}
-          <span className={chatMessageStyle.timeStamp}>{timestamp}</span>
-        </h4>
-        <p>{message}</p>
-      </div>
-    </div>
+    <>
+      {user?.uid === userInfo?.uid ? (
+        <div className={chatMessageStyle.message}>
+          <div className={chatMessageStyle.messageInfoRight}>
+            <h4>
+              {userInfo?.name}
+              <span className={chatMessageStyle.timeStamp}>{timestamp}</span>
+            </h4>
+            <p>{message}</p>
+          </div>
+        </div>
+      ) : (
+        <div className={chatMessageStyle.message}>
+          <Image
+            className={chatMessageStyle.userIcon}
+            src={userInfo?.icon}
+            alt=""
+            width={60}
+            height={60}
+          />
+          <div className={chatMessageStyle.messageInfo}>
+            <h4>
+              {userInfo?.name}
+              <span className={chatMessageStyle.timeStamp}>{timestamp}</span>
+            </h4>
+            <p>{message}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
