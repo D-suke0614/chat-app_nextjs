dcddb9faa20e9167081f8efc1c020a57a4ed14fd
Daisuke <d.suke.kida@gmail.com>
2023-09-26 23:32:16 +0900

    🔥不要なimportの削除

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import headerStyle from "./Header.module.scss";
-import { Button } from "@mui/material";
 import { FirebaseError } from "firebase/app";
 import { getAuth, signOut } from "firebase/auth";
 import { useAuthContext } from "@/feature/auth/provider/AuthProvider";
@@ -22,7 +21,6 @@ function Header() {
   };
 
   return (
-    // [TODO] 仮置きしているスタイルの修正
     <div className={headerStyle.header}>
       {user ? (
         <>
