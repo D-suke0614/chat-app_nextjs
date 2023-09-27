39e31daa17448b51a181426676c5263b6b739940
Daisuke <d.suke.kida@gmail.com>
2023-09-28 01:59:27 +0900

    🔥 不要なimportの削除

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import chatMessageStyle from "./ChatMessage.module.scss";
 import Image from "next/image";
 import { useAuthContext } from "@/feature/auth/provider/AuthProvider";
