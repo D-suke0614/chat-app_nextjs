328148945c21eb1ba6bf8569c6199c087a78318b
Daisuke <d.suke.kida@gmail.com>
2023-10-06 00:21:59 +0900

    🚧 サイドバー開発中

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import sidebarStyle from "./Sidebar.module.scss";
+
+function Sidebar() {
+  return (
+    <>
+      <div className={sidebarStyle.sidebar}>Sidebar</div>
+    </>
+  );
+}
+
+export default Sidebar;
