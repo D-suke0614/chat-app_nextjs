8bbe7d1b2d1da9de891cab47a352ffa5389909f7
Daisuke <d.suke.kida@gmail.com>
2023-10-08 08:56:04 +0900

    🚧 サイドバー作成中

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,10 +1,53 @@
 import React from "react";
 import sidebarStyle from "./Sidebar.module.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function Sidebar() {
   return (
     <>
-      <div className={sidebarStyle.sidebar}>Sidebar</div>
+      <div className={sidebarStyle.sidebar}>
+        <div className={sidebarStyle.sidebarTop}>
+          <h3>Chat App</h3>
+          <ExpandMoreIcon />
+        </div>
+
+        <div className={sidebarStyle.channels}>
+          <div className={sidebarStyle.sidebarChannelsHeader}>
+            <div className={sidebarStyle.sidebarHeader}>
+              <ExpandMoreIcon />
+              <h4>Programming Channel</h4>
+            </div>
+            <AddIcon
+              className={sidebarStyle.sidebarAddIcon}
+              onClick={() => {}}
+            />
+          </div>
+
+          <div className={sidebarStyle.sidebarChannelList}>
+            {/* <SidebarChannel /> */}
+          </div>
+        </div>
+
+        <div className={sidebarStyle.sidebarFooter}>
+          <div className={sidebarStyle.sidebarAccount}>
+            <img src="" alt="sidebar account" />
+            <div className={sidebarStyle.accountName}>
+              <h4>D.suke</h4>
+              <span>#1234</span>
+            </div>
+          </div>
+
+          <div className={sidebarStyle.sidebarVoice}>
+            <MicIcon />
+            <HeadphonesIcon />
+            <SettingsIcon />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
